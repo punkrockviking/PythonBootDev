1686dab89151708c3283fc9f0048edfe6daaea23
Dan Eccles <79684850+punkrockviking@users.noreply.github.com>
2023-05-11 14:01:44 -0600

    Update IntroNotes.py

--- IntroNotes.py
+++ IntroNotes.py
@@ -1,2 +1,32 @@
 # Introduction to Python
 
+"""
+comment out blocks of code using the triple quotes before and after
+"""
+
+"""
+sword_damage = 10
+player_health = 100
+health_after_attack = player_health - sword_damage
+
+# Don't touch below this line
+print(f"Lollilfred's health is: {player_health}")
+print(f"Lollilfred is hit by a sword for {sword_damage} damage...")
+print(f"Lollilfred's health is now: {health_after_attack}")
+
+"""
+"""
+# print to the console
+print("Use the arrow keys to move")
+"""
+
+# Python is not often used in front end apps
+
+# you can print numbers too
+# print(250 + 75)
+
+# Code runs in order, starting at the top of the program.
+
+
+
+
