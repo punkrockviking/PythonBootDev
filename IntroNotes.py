bb33e874e08c8b39e739e05ae6b2c89b659f281e
Dan Eccles <79684850+punkrockviking@users.noreply.github.com>
2023-05-11 14:11:03 -0600

    Update IntroNotes.py

--- IntroNotes.py
+++ IntroNotes.py
@@ -27,6 +27,44 @@ print("Use the arrow keys to move")
 
 # Code runs in order, starting at the top of the program.
 
+# use ctrl key + up/down to move a line of code up/down
 
+# syntax is valid code in programming. ex: Print() is incorrect but print() is correct
+
+"""
+Exercises:
+
+1
+incorrect code:
+print("Starting up server...')
+prnt("local server is listening on port 8080")
+
+corrected code:
+print("Starting up server...")
+print("local server is listening on port 8080")
+
+2
+print("(250 + 241 + 244 + 255) / 4")
+
+print((250 + 241 + 244 + 255) / 4)
+
+3
+items_in_cart = 5
+cost_of_each_item = 2.50
+total_cost = items_in_cart * cost_of_each_item
+
+# Don't Touch Above This line
+
+print "Thank you for shopping at Bootmart!"
+print ""
+
+# Don't Touch Below This Line
+
+print(f"${total_cost:.2f}")
+
+corrected code: 
+print("Thank you for shopping at Bootmart!")
+print("Your total today was:")
+"""
 
 
